a70ec55c8d10279dc219d290c7464b7953460692
devsnowin <snowin.developer@gmail.com>
2023-02-27 00:59:45 +0530

    version 2

--- src/components/layouts/header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Header() {
-	return (
-		<div className="shadow-slate-200 shadow-lg p-4 flex justify-center items-center md:p-6">
-			<h1 className="text-2xl font-monoton md:text-4xl">Galeria</h1>
-		</div>
-	)
-}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,12 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        poppins: ['Poppins', 'san-serif'],
-        monoton: ['Monoton', 'cursive'],
-      }
-    },
-  },
-  plugins: [],
-}
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				poppins: ['Poppins', 'san-serif'],
+				monoton: ['Monoton', 'cursive'],
+			},
+		},
+	},
+	plugins: [],
+};
